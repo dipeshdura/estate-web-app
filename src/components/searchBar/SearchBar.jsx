918f299d2d2e4c8ad2a221fc2b6435394cae92d4
dipeshdura <dipeshdura14@gmail.com>
2025-04-30 15:15:43 +0545

    homePage

---
+++ src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import './searchBar.scss';
+import search from "../../assets/search.png";
+
+const types =["buy","rent"];
+const SearchBar = () => {
+    const [query, setQuery]=useState({
+        type:"buy",
+        location:"",
+        minPrice:0,
+        maxPrice:0
+    });
+
+    const switchType =(val)=>{
+        setQuery((prev)=>({...prev,type:val}))
+    };
+  return (
+    <div className='searchBar'>
+
+        <div className="type">
+            {types.map((type)=>(
+                <button key={type}
+                onClick={()=>switchType(type)}
+                className={query.type==type ? "active":""}>
+                    {type}
+                    </button>
+            ))}
+        </div>
+
+        <form action="">
+            <input type="text" name="location" placeholder="City Location" />
+            
+            <input 
+            type="number"
+            name="minPrice"
+            min={0}
+            max={10000000}
+            placeholder="Min Price" />
+
+            <input type="number"
+            min={0}
+            max={10000000}
+            name="maxPrice"
+            placeholder="Max Price" />
+
+            <button>
+                <img src={search} alt={search} />
+            </button>
+        </form>
+    </div>
+  )
+}
+
+export default SearchBar
---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./homePage.scss";
+import bg from "../../assets/bg.png";
+import SearchBar from "../../components/searchBar/SearchBar";
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis
+            totam asperiores et omnis dolores dolorum quasi, fuga sequi dolorem
+            quod!
+          </p>
+
+          <SearchBar />
+
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>1200+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="imgContainer">
+        <img src={bg} alt={bg} />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
